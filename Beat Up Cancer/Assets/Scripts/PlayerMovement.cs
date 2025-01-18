dba342630b96e371cd71d9046ad8e2948879b07c
scanlona <59579777+scanlona@users.noreply.github.com>
2025-01-18 14:59:26 -0800

    v1.00
    Added:
    -background stage color,
    -more platforms,
    -character jump feature (barebones),
    -a pause and home button on the UI (not yet implemented) (icon graphics made by me).
    -Fixed bug where character would rotate unexpectedly on z-axis.

--- Beat Up Cancer/Assets/Scripts/PlayerMovement.cs
+++ Beat Up Cancer/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,20 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D body;
     float speed = 10;
+    private float jumpingPower = 5f;
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
     }
 
     void Update()
-    {
+    {   
+        // 2D Movement
         body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            body.linearVelocity = new UnityEngine.Vector2(body.linearVelocity.x, jumpingPower);
+        }        
     }
 }
