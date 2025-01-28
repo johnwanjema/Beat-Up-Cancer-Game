e4ee69cf16a013b2d876ca2418c6afabb520a00b
Wanjema <jonwanjema@gmail.com>
2025-01-28 11:54:33 -0800

    add esophageal cancer cell as enemy

---
+++ Beat Up Cancer/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [HideInInspector]
+    public  float speed;
+
+    private Rigidbody2D mybody;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mybody =  GetComponent<Rigidbody2D>();
+
+        speed = 5f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // velocity as a force to move player up
+        // move enemy on the x- axis 
+        mybody.linearVelocity = new Vector2(speed,mybody.linearVelocity.y);
+    }
+}
