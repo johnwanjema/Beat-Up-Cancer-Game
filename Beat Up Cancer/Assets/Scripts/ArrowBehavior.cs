31920dca6867edc43b5e611c06f7734fdcd89307
Wanjema <jonwanjema@gmail.com>
2025-02-19 15:23:48 -0800

    add logic for arrows to kill cancer cells

---
+++ Beat Up Cancer/Assets/Scripts/ArrowBehavior.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ArrowBehavior : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            // Destroy the enemy object
+            Destroy(collision.gameObject);
+
+            // Destroy the arrow after hitting the enemy
+            Destroy(gameObject);
+        }
+    }
+}
