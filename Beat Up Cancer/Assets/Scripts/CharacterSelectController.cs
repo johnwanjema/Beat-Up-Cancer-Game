a2ee5541ed02aa5b47ab45c6b8b9d053a167f4aa
Wanjema <jonwanjema@gmail.com>
2025-04-02 16:44:45 -0700

    remove debug logs for character select screen

--- Beat Up Cancer/Assets/Scripts/CharacterSelectController.cs
+++ Beat Up Cancer/Assets/Scripts/CharacterSelectController.cs
@@ -9,7 +9,6 @@ public class characterSelect : MonoBehaviour
             int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
 
         GameManager.instance.charIndex = selectedCharacter;
-        Debug.Log("yes bana " +  GameManager.instance.charIndex);
        
         SceneManager.LoadScene("Level 1");
     
