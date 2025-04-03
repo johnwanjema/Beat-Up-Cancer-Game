d86d0e8f29f9b0a8d1f80f6a1220327a0af3ce3a
Wanjema <jonwanjema@gmail.com>
2025-04-02 17:01:22 -0700

    kill cancer cell on collision with fireball

--- Beat Up Cancer/Assets/Scripts/FireBall.cs
+++ Beat Up Cancer/Assets/Scripts/FireBall.cs
@@ -33,12 +33,13 @@ public class Fireball : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.CompareTag("Enemy"))
         {
             Enemy enemy = collision.GetComponent<Enemy>();
             if (enemy != null)
             {
-                enemy.TakeDamage(damage);
+                enemy.TakeDamage(50);
             }
             Destroy(gameObject); // Destroy fireball on impact
         }
