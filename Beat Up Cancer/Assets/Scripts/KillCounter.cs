57b82f7bfcdfe2b2d26785ad3b9c7d0beaca4235
scanlona <59579777+scanlona@users.noreply.github.com>
2025-04-11 13:27:45 -0700

    Added Working Victory Screen + Victory Conditions!
    Added feature so that when the goal number of enemies defeated is reached within a level, the victory screen will pop up and end the level.

--- Beat Up Cancer/Assets/Scripts/KillCounter.cs
+++ Beat Up Cancer/Assets/Scripts/KillCounter.cs
@@ -4,6 +4,7 @@ using TMPro;
 public class KillCounter : MonoBehaviour
 {
     public TMP_Text killCounter;
+    public GameObject VictoryPanel;
     public static int kills = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,5 +16,10 @@ public class KillCounter : MonoBehaviour
     void Update()
     {
         killCounter.text = string.Format("Cancer Cells Killed: {0}/10", kills);
+        if (kills >= 10) 
+        {
+            VictoryPanel.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 }
