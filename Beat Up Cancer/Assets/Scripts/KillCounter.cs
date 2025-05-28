35e079a13274a836905e98e547abb660f23afb97
scanlona <59579777+scanlona@users.noreply.github.com>
2025-05-28 11:57:31 -0700

    Ingame Points Counter, Fully Functioning! & Level 3 Kill Count Added
    Created UI and backend implementation for the ingame score counter, to keep track of your current score in the level and display it onscreen alongside your number of enemy eliminations. ALSO, even implementing this for Level 3, and modified the level complete elimination goal to 99 enemies instead of the prior 10, to simulate a final boss rush and a higher skill ceiling to beat, and to draw out playtime for the last level

--- Beat Up Cancer/Assets/Scripts/KillCounter.cs
+++ Beat Up Cancer/Assets/Scripts/KillCounter.cs
@@ -1,25 +1,42 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class KillCounter : MonoBehaviour
 {
     public TMP_Text killCounter;
+    public TMP_Text ingameScore;
     public GameObject VictoryPanel;
     public static int kills = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        killCounter.text = string.Format("Cancer Cells Killed: {0}/10", kills);
-        if (kills >= 10) 
+        if (SceneManager.GetActiveScene().name == "Level 3")
         {
-            VictoryPanel.SetActive(true);
-            Time.timeScale = 0;
+            killCounter.text = string.Format("Cancer Cells Killed: {0}/99", kills);
+            ingameScore.text = string.Format("Score: {0}", kills * 500);
+            if (kills >= 99)
+            {
+                VictoryPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+
+        else
+        {
+            killCounter.text = string.Format("Cancer Cells Killed: {0}/10", kills);
+            ingameScore.text = string.Format("Score: {0}", kills * 500);
+            if (kills >= 10)
+            {
+                VictoryPanel.SetActive(true);
+                Time.timeScale = 0;
+            }
         }
     }
 }
